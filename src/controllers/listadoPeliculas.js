02e521a89c207d62017a5e748cbffc28f01f0ba6
407 <407@academicos.cesde.edu.co>
2022-11-15 16:50:01 -0500

    Se crean posters y vista

--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -40,33 +40,43 @@ let peliculas=[
         poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
     },
     {
-        nombre:"Minions2",
-        duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        nombre:"Cenicienta",
+        duracion:110,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/Cenicienta.png?alt=media&token=3ff9f23e-a454-4e31-89e9-b82b902477e7"
     },
     {
-        nombre:"Minions2",
-        duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        nombre:"La Roya",
+        duracion:115,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20Roya.jpg?alt=media&token=376921b1-be97-4469-990e-0fb77a7ad85f"
     },
     {
-        nombre:"Minions2",
-        duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        nombre:"La gran tormenta",
+        duracion:125,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20gran%20tormenta.jpg?alt=media&token=0d75703d-b6de-40b1-9622-cfecd7b65104"
     },
     {
-        nombre:"Minions2",
-        duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        nombre:"La luz del diablo",
+        duracion:105,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20luz%20del%20diablo.png?alt=media&token=deea2251-353c-4ddc-bbe8-e56ab337a2a5"
     },
     {
-        nombre:"Minions2",
-        duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        nombre:"Lilo Cocodrilo",
+        duracion:125,
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/cocodrilo.jpg?alt=media&token=bc639a13-ad13-4cbf-a955-247a3c85fd5f"
+    },
+    {
+        nombre:"La Maldicion",
+        duracion:"98",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/la%20maldicion.png?alt=media&token=85b34487-9897-406b-b097-6976b422ace1"
+    },
+    {
+        nombre:"Mar de Sangre",
+        duracion:"116",
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/mar%20de%20sangre.png?alt=media&token=80291a09-3155-4198-872c-4bb8e45879cb"
     }
 ]
 
-let fila=document.getElementById("fila")
+/*let fila=document.getElementById("fila")
 
 peliculas.forEach(function(pelicula){
     console.log(pelicula.poster)
@@ -76,4 +86,44 @@ peliculas.forEach(function(pelicula){
     foto.classList.add("img-fluid","w-100")
 
     fila.appendChild(foto)
+})*/
+
+//Creamos una referencia al espacio HTML donde queremos
+//hacer el render (PINTAR ETIQUETAS)
+let etiquetaFila=document.getElementById("fila")
+
+//SI YA TENDO DATOS QUE NORMALMENTE LLEGAN AL FRONT COMO
+//UN ARREGLO DE OBJETOS DEBEMOS RECORRER Y DEPURAR DICHA
+//INFORMACION
+peliculas.forEach(function(pelicula){
+    //PARA PINTAR LA INFORMACION DE CADA PELICULA 
+    //DEBEMOS APLICAR UNA TECNICA CONOCITA COMO TRAVERSING
+    //TRAVERSING=CREAR ETIQUETAS DE HTML DESDE JS
+    let columna=document.createElement("div")
+    columna.classList.add("col")
+
+
+    let tarjeta=document.createElement("div")
+    tarjeta.classList.add("card","h-100","shadow") //estilos
+
+    let poster=document.createElement("img")
+    poster.classList.add("img-fluid","w-100") //estilos
+    poster.src=pelicula.poster
+
+    let nombrePelicula=document.createElement("h2")
+    nombrePelicula.classList.add("text-center","fw-bold")  //estilos
+    nombrePelicula.textContent=pelicula.nombre
+
+    let duracionPelicula=document.createElement("h5")
+    duracionPelicula.classList.add("text-start")
+    duracionPelicula.textContent="Duracion: "+pelicula.duracion+" Min"
+
+    //PADRES E HIJOS (GERARQUIA DE ETIQUETAS)
+
+    etiquetaFila.appendChild(columna)
+    columna.appendChild(tarjeta)
+    tarjeta.appendChild(poster)
+    tarjeta.appendChild(nombrePelicula)
+    tarjeta.appendChild(duracionPelicula)
+    
 })
