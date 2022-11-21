80ef36f3c382ebf0550a78dc0e143336a52f8152
jemunoz@cede.net <jemunozmon@cesde.net>
2022-11-21 16:39:11 -0500

    actualizacion 21/11//22

--- src/controllers/listadoPeliculas.js
+++ src/controllers/listadoPeliculas.js
@@ -2,77 +2,92 @@ let peliculas=[
     {
         nombre:"Amsterdam",
         duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/AMSTERDAMNUEVO1.jpg?alt=media&token=45a43f89-3af7-4b27-8f78-8b0d21d9fa2c"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/AMSTERDAMNUEVO1.jpg?alt=media&token=45a43f89-3af7-4b27-8f78-8b0d21d9fa2c",
+        sinopsis:"En la década de 1930, tres amigos que presencian un asesinato se convierten en sospechosos del mismo. La investigación que emprenden los lleva a descubrir una de las conspiraciones más asombrosas de la historia estadounidense."
     },
     {
         nombre:"Blackadam",
         duracion:140,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/Blackadam.jpg?alt=media&token=53c41265-872c-425e-b86e-f5d1bb6e5730"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/Blackadam.jpg?alt=media&token=53c41265-872c-425e-b86e-f5d1bb6e5730",
+        sinopsis:"Unos arqueólogos liberan de su tumba a Black Adam, quien llevaba 5000 años preso tras haber recibido los poderes de los dioses. De nuevo entre los humanos, Black Adam se dispone a imponer su justicia, muy diferente a la del mundo en el que despertó."
     },
     {
         nombre:"Supermascotas",
         duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/SUPERMASCOTAS.jpg?alt=media&token=15f14f8e-d02c-4a2b-88b8-3b9331d0e64b"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/SUPERMASCOTAS.jpg?alt=media&token=15f14f8e-d02c-4a2b-88b8-3b9331d0e64b",
+        sinopsis:"El superperro Krypto une a un grupo de divertidos y descerebrados animales para que lo ayuden a rescatar a los superhéroes de la Liga de la Justicia, que han sido secuestrados."
     },
     {
         nombre:"Hallowen",
         duracion:120,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/hallowenultimo.jpg?alt=media&token=74e6095d-ee13-4124-b9d6-d498eede57ec"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/hallowenultimo.jpg?alt=media&token=74e6095d-ee13-4124-b9d6-d498eede57ec",
+        sinopsis:"Cuatro años después de la desaparición de Michael Myers, el joven Corey lo encuentra maltrecho en las alcantarillas. Poco a poco, se establece un extraño vínculo entre estos dos personajes unidos por la venganza y la locura."
     },
     {
         nombre:"OnePiece",
         duracion:110,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/ONEPIECE1.jpg?alt=media&token=5282128c-53bd-432d-80b3-a988fc6bcabc"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/ONEPIECE1.jpg?alt=media&token=5282128c-53bd-432d-80b3-a988fc6bcabc",
+        sinopsis:"Uta, la cantante más popular del mundo, va a presentarse en un escenario y revelar su apariencia por primera vez. Luffy y sus amigos acuden al concierto y se dan cuenta de que la voz de Uta es capaz de cambiarlo todo."
     },
     {
         nombre:"Sonrie",
         duracion:90,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/SONRIE1.jpg?alt=media&token=127ca637-7cc8-4ad3-8b66-c6ea9b398d76"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/SONRIE1.jpg?alt=media&token=127ca637-7cc8-4ad3-8b66-c6ea9b398d76",
+        sinopsis:"Tras presencia el dramático incidente sufrido por un paciente, la Dra. Cotter empieza a experimentar hechos aterradores sin explicación aparente. A medida que el horror se adueña de su vida, comprende que la respuesta está en su propio pasado."
     },
     {
         nombre:"Jeeppers",
         duracion:98,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/jeeppers.jpg?alt=media&token=9cee5671-3e1b-4465-863d-08eed87900a9"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/jeeppers.jpg?alt=media&token=9cee5671-3e1b-4465-863d-08eed87900a9",
+        sinopsis:"Laine y Chase acuden a un festival temático de horror. Allí, ella empieza a tener premoniciones que auguran el retorno de un monstruo alado que despierta cada 23 años sediento de sangre."
     },
     {
         nombre:"Minions2",
         duracion:100,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/minions2.jpg?alt=media&token=c7a81d41-7a9e-49b6-89b7-0ba42ae7f591",
+        sinopsis:'En los años 70, Gru crece siendo un gran admirador de "Los salvajes seis", un supergrupo de villanos. Para demostrarles que puede ser malvado, diseña un plan con la esperanza de formar parte de la banda.'
     },
     {
         nombre:"Cenicienta",
         duracion:110,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/Cenicienta.png?alt=media&token=3ff9f23e-a454-4e31-89e9-b82b902477e7"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/Cenicienta.png?alt=media&token=3ff9f23e-a454-4e31-89e9-b82b902477e7",
+        sinopsis:"La idílica vida de la preciosa Ella se viene abajo cuando, tras la muerte de su madre, su padre se vuelve a casar. La joven quiere apoyar a su padre y acoge con cariño en la casa familiar a su madrastra y a sus hijas, Anastasia y Drisella. Sin embargo, cuando el padre de Ella muere inesperadamente, la joven se encuentra a merced de una nueva familia celosa y cruel."
     },
     {
         nombre:"La Roya",
         duracion:115,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20Roya.jpg?alt=media&token=376921b1-be97-4469-990e-0fb77a7ad85f"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20Roya.jpg?alt=media&token=376921b1-be97-4469-990e-0fb77a7ad85f",
+        sinopsis:"Jorge es un joven campesino quien vive en lo alto de una montaña. Todos los de su generación han emigrado a la ciudad, pero él se ha quedado para hacerse cargo de la finca cafetera que ha heredado de su padre, mientras cuida de su abuelo y tiene amoríos con su prima Rosa."
     },
     {
         nombre:"La gran tormenta",
         duracion:125,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20gran%20tormenta.jpg?alt=media&token=0d75703d-b6de-40b1-9622-cfecd7b65104"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20gran%20tormenta.jpg?alt=media&token=0d75703d-b6de-40b1-9622-cfecd7b65104",
+        sinopsis:"Cuando su avión se estrella en una remota montaña cubierta de nieve, Jane y Paul tienen que luchar por sus vidas como los únicos sobrevivientes que quedan. Juntos se embarcan en un angustioso viaje fuera del desierto."
     },
     {
         nombre:"La luz del diablo",
         duracion:105,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20luz%20del%20diablo.png?alt=media&token=deea2251-353c-4ddc-bbe8-e56ab337a2a5"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/La%20luz%20del%20diablo.png?alt=media&token=deea2251-353c-4ddc-bbe8-e56ab337a2a5",
+        sinopsis:"Según informes reales del Vaticano, los casos de posesión demoníaca han aumentado significativamente en los últimos años. En respuesta, la iglesia católica ha reabierto en secreto escuelas de exorcismo para capacitar a los sacerdotes en este rito sagrado."
     },
     {
         nombre:"Lilo Cocodrilo",
         duracion:125,
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/cocodrilo.jpg?alt=media&token=bc639a13-ad13-4cbf-a955-247a3c85fd5f"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/cocodrilo.jpg?alt=media&token=bc639a13-ad13-4cbf-a955-247a3c85fd5f",
+        sinopsis:"La familia Primm se muda a Nueva York, donde Josh, el pequeño de la casa, tiene problemas para adaptarse a la escuela y hacer amigos. Todo cambia cuando Josh conoce a Lilo, un cocodrilo al que le apasiona la música, bañarse y el caviar."
     },
     {
         nombre:"La Maldicion",
         duracion:"98",
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/la%20maldicion.png?alt=media&token=85b34487-9897-406b-b097-6976b422ace1"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/la%20maldicion.png?alt=media&token=85b34487-9897-406b-b097-6976b422ace1",
+        sinopsis:"Después de que una joven madre asesina a su familia en su propia casa, una madre soltera y un detective intentan investigar y resolver el caso. Más tarde, descubren que la casa está maldita por un fantasma vengativo que condena a quienes entran con una muerte violenta."
     },
     {
         nombre:"Mar de Sangre",
         duracion:"116",
-        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/mar%20de%20sangre.png?alt=media&token=80291a09-3155-4198-872c-4bb8e45879cb"
+        poster:"https://firebasestorage.googleapis.com/v0/b/cinemaalbert-3aa79.appspot.com/o/mar%20de%20sangre.png?alt=media&token=80291a09-3155-4198-872c-4bb8e45879cb",
+        sinopsis:"La historia de las correrías sangrientas de los piratas que atacaron Cartagena de Indias y de sus heroicos defensores. El mar Caribe fue el escenario por excelencia de confrontación por las riquezas del Nuevo Mundo."
     }
 ]
 
@@ -118,6 +133,11 @@ peliculas.forEach(function(pelicula){
     duracionPelicula.classList.add("text-start")
     duracionPelicula.textContent="Duracion: "+pelicula.duracion+" Min"
 
+    let sinopsisPelicula=document.createElement("p")
+    sinopsisPelicula.classList.add("d-none")
+    sinopsisPelicula.textContent=pelicula.sinopsis
+
+
     //PADRES E HIJOS (GERARQUIA DE ETIQUETAS)
 
     etiquetaFila.appendChild(columna)
@@ -125,5 +145,30 @@ peliculas.forEach(function(pelicula){
     tarjeta.appendChild(poster)
     tarjeta.appendChild(nombrePelicula)
     tarjeta.appendChild(duracionPelicula)
+    tarjeta.appendChild(sinopsisPelicula)
     
+})
+
+
+//Escuchando el evento de clic en alguna pelicula de la cartelera:
+let peliculaSeleccionada={}
+etiquetaFila.addEventListener("click",function(evento){
+    /*alert("Estas seleccionando una pelicula...") ejemplo*/
+    //console.log(evento.target.classList.contains("img-fluid"))
+    if(evento.target.classList.contains("img-fluid")){
+        //alert("Estas seleccionando una pelicula")
+        
+        //Obteniendo los datos de la pelicula
+        peliculaSeleccionada.imagen=evento.target.parentElement.querySelector('img').src           //console.log(evento.target.parentElement.querySelector('img').src)
+        peliculaSeleccionada.titulo=evento.target.parentElement.querySelector('h2').textContent    //console.log(evento.target.parentElement.querySelector('h2').textContent)
+        peliculaSeleccionada.duracion=evento.target.parentElement.querySelector('h5').textContent  //console.log(evento.target.parentElement.querySelector('h5').textContent)
+        peliculaSeleccionada.sinopsisPelicula=evento.target.parentElement.querySelector("p").textContent
+        
+                                                                                                        //console.log(peliculaSeleccionada)
+        //Enviado nuestros datos de la pelicula seleccionada a la memoria
+        localStorage.setItem("pelicula",JSON.stringify(peliculaSeleccionada))   
+        
+        
+        window.location.href="./src/views/verinfopelicula.html"
+    }
 })
